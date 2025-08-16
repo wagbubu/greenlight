7169f7261aa8d0cabca1a465a35a72ce8ee290b0
Raven <raven.guirao@ph.c3-interactive.com>
2025-08-16 11:12:39 +0800

    Full text search adding index

--- migrations/000002_add_movies_check_constraints.up.sql
+++ migrations/000002_add_movies_check_constraints.up.sql
@@ -1,5 +1,14 @@
-ALTER TABLE movies ADD CONSTRAINT movies_runtime_check CHECK (runtime >= 0);
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'movies_runtime_check') THEN
+        ALTER TABLE movies ADD CONSTRAINT movies_runtime_check CHECK (runtime >= 0);
+    END IF;
 
-ALTER TABLE movies ADD CONSTRAINT movies_year_check CHECK (year BETWEEN 1888 AND date_part('year', now()));
+    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'movies_year_check') THEN
+        ALTER TABLE movies ADD CONSTRAINT movies_year_check CHECK (year BETWEEN 1888 AND date_part('year', now()));
+    END IF;
 
-ALTER TABLE movies ADD CONSTRAINT genres_length_check CHECK (array_length(genres, 1) BETWEEN 1 AND 5);
+    IF NOT EXISTS (SELECT 1 FROM pg_constraint WHERE conname = 'genres_length_check') THEN
+        ALTER TABLE movies ADD CONSTRAINT genres_length_check CHECK (array_length(genres, 1) BETWEEN 1 AND 5);
+    END IF;
+END$$;
