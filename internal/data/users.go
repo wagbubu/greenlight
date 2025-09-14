53eca574131cdda2be74de308f0e728d5f905f7e
Raven <raven.guirao@ph.c3-interactive.com>
2025-09-14 10:47:52 +0800

    activating users

--- internal/data/users.go
+++ internal/data/users.go
@@ -2,6 +2,7 @@ package data
 
 import (
 	"context"
+	"crypto/sha256"
 	"database/sql"
 	"errors"
 	"time"
@@ -79,12 +80,13 @@ func (m UserModel) Update(user *User) error {
 	query := `
 		Update users
 		SET name = $1, email = $2, password_hash = $3, activated = $4, version = version + 1
-		WHERE id = $5 AND version $6
+		WHERE id = $5 AND version = $6
 		RETURNING version
 	`
 	args := []any{
 		user.Name,
 		user.Email,
+		user.Password.hash,
 		user.Activated,
 		user.ID,
 		user.Version,
@@ -93,7 +95,7 @@ func (m UserModel) Update(user *User) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	err := m.DB.QueryRowContext(ctx, query, args).Scan(&user.ID, &user.Name, &user.Version)
+	err := m.DB.QueryRowContext(ctx, query, args...).Scan(&user.Version)
 	if err != nil {
 		switch {
 		case err.Error() == `pq: duplicate key value unique constraint "users_email_key"`:
@@ -104,6 +106,45 @@ func (m UserModel) Update(user *User) error {
 	}
 
 	return nil
+}
+
+func (m UserModel) GetForToken(scope string, tokenPlainText string) (*User, error) {
+	tokenHash := sha256.Sum256([]byte(tokenPlainText))
+
+	queryStmt := `SELECT users.id, users.created_at, users.name, users.email, users.password_hash, users.activated, users.version
+								FROM users
+								INNER JOIN tokens
+								ON users.id = tokens.user_id
+								WHERE tokens.hash = $1
+								AND tokens.scope = $2
+								AND tokens.expiry > $3`
+	args := []interface{}{tokenHash[:], scope, time.Now()}
+
+	var user User
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err := m.DB.QueryRowContext(ctx, queryStmt, args...).Scan(
+		&user.ID,
+		&user.CreatedAt,
+		&user.Name,
+		&user.Email,
+		&user.Password.hash,
+		&user.Activated,
+		&user.Version,
+	)
+
+	if err != nil {
+		switch {
+		case errors.Is(err, sql.ErrNoRows):
+			return nil, ErrRecordNotFound
+		default:
+			return nil, err
+		}
+	}
+
+	return &user, nil
 }
 
 type User struct {
